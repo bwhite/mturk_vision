9481c45aec76627a923bb37186466996b9a90092
Brandyn A. White <bwhite@dappervision.com>
2012-02-16 09:33:59 -0500

    Updated
    Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

--- amt_video/tools/amt_create_hit.py
+++ amt_video/tools/amt_create_hit.py
@@ -6,15 +6,15 @@ kw = {}
 #kw = {'host': 'mechanicalturk.sandbox.amazonaws.com'}  # Uncomment for sandbox
 mtc = boto.connect_mturk(os.environ['AWS_ACCESS_KEY_ID'], os.environ['AWS_SECRET_ACCESS_KEY'], **kw)
 
-question = boto.mturk.question.ExternalQuestion('http://api.dappervision.com:8001', 1500)
+question = boto.mturk.question.ExternalQuestion('http://api.dappervision.com:8000', 2000)
 qualifications = boto.mturk.qualification.Qualifications()
-qualifications.add(boto.mturk.qualification.PercentAssignmentsApprovedRequirement('GreaterThan', 80, True))
-qualifications.add(boto.mturk.qualification.NumberHitsApprovedRequirement('GreaterThan', 100, True))
+#qualifications.add(boto.mturk.qualification.PercentAssignmentsApprovedRequirement('GreaterThan', 80, True))
+#qualifications.add(boto.mturk.qualification.NumberHitsApprovedRequirement('GreaterThan', 100, True))
 out = mtc.create_hit(question=question,
-                     max_assignments=4,
+                     max_assignments=5,
                      qualifications=qualifications,
-                     title='',
-                     description='Select which video (shown as image keyframes) corresponds to a text description.',
-                     keywords='video annotation quick fun game'.split(),
-                     duration=int(60 * 30),
-                     reward=0.0)
+                     title='Write a text description for a sequence of video frames.',
+                     description='Write a text description for a sequence of video frames.',
+                     keywords='video annotation'.split(),
+                     duration=int(30 * 25),
+                     reward=0.1)
