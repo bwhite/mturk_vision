421fbd8702d5b523f0604d6b996d86d6287e962f
Brandyn A. White <bwhite@dappervision.com>
2013-05-02 16:37:20 -0400

    Updated
    Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

--- mturk_vision/__init__.py
+++ mturk_vision/__init__.py
@@ -6,6 +6,7 @@ from base import AMTManager, UserNotFinishedException
 from video import AMTVideoClassificationManager, AMTVideoTextMatchManager, AMTVideoDescriptionManager
 from image import AMTImageClassificationManager
 from image_entity import AMTImageEntityManager
+from image_class import AMTImageClassManager
 from image_segments import AMTImageSegmentsManager
 from image_query import AMTImageQueryManager
 from image_query_batch import AMTImageQueryBatchManager
