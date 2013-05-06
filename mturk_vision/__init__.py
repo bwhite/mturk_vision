ea146e412f822b0640c747ad1b2c58e836391f13
Brandyn A. White <bwhite@dappervision.com>
2013-05-06 13:12:47 -0400

    Initial stage of refactor
    Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

--- mturk_vision/__init__.py
+++ mturk_vision/__init__.py
@@ -3,12 +3,8 @@ def quote(x):
     return cgi.escape(x, quote=True)
 
 from base import AMTManager, UserNotFinishedException
-from video import AMTVideoClassificationManager, AMTVideoTextMatchManager, AMTVideoDescriptionManager
 from image import AMTImageClassificationManager
-from image_entity import AMTImageEntityManager
 from image_class import AMTImageClassManager
-from image_segments import AMTImageSegmentsManager
-from image_query import AMTImageQueryManager
 from image_query_batch import AMTImageQueryBatchManager
 from tools import create_hit
 from manager import manager
--- mturk_vision/manager.py
+++ mturk_vision/manager.py
@@ -8,44 +8,16 @@ ROOT = os.path.abspath(__path__[0])
 
 
 def manager(**args):
-    db_nums = list(enumerate(['users', 'key_to_path', 'path_to_key', 'frame', 'description', 'image', 'response', 'state']))
+    db_nums = list(enumerate(['users', 'response', 'data', 'state', 'key_to_path', 'path_to_key']))
     logging.debug(db_nums)
     args.update(dict((y + '_db', redis.StrictRedis(host=args['redis_address'], port=args['redis_port'], db=x))
                      for x, y in db_nums))
     logging.debug(args)
     sp = lambda x: ROOT + '/static_private/' + x
     args['data_source'] = data_source_from_uri(args['data'])
-    if args['type'] == 'video_label':
-        m = mturk_vision.AMTVideoClassificationManager(index_path=sp('video_label.html'),
-                                                       config_path=sp('video_label_config.js'),
-                                                       **args)
-    elif args['type'] == 'video_match':
-        m = mturk_vision.AMTVideoTextMatchManager(index_path=sp('video_match.html'),
-                                                  config_path=sp('video_match_config.js'),
-                                                  **args)
-    elif args['type'] == 'video_description':
-        m = mturk_vision.AMTVideoDescriptionManager(index_path=sp('video_description.html'),
-                                                    config_path=sp('video_description_config.js'),
-                                                    **args)
-    elif args['type'] == 'image_label':
-        m = mturk_vision.AMTImageClassificationManager(index_path=sp('video_label.html'),
-                                                       config_path=sp('image_label_config.js'),
-                                                       **args)
-    elif args['type'] == 'image_entity':
-        m = mturk_vision.AMTImageEntityManager(index_path=sp('video_label.html'),
-                                               config_path=sp('image_entity_config.js'),
-                                               **args)
-    elif args['type'] == 'image_class':
+    if args['type'] == 'image_class':
         m = mturk_vision.AMTImageClassManager(index_path=sp('video_label.html'),
                                               config_path=sp('image_class_config.js'),
-                                              **args)
-    elif args['type'] == 'image_segments':
-        m = mturk_vision.AMTImageSegmentsManager(index_path=sp('image_segments.html'),
-                                                 config_path=sp('image_segments_config.js'),
-                                                 **args)
-    elif args['type'] == 'image_query':
-        m = mturk_vision.AMTImageQueryManager(index_path=sp('video_label.html'),
-                                              config_path=sp('image_query_config.js'),
                                               **args)
     elif args['type'] == 'image_query_batch':
         m = mturk_vision.AMTImageQueryBatchManager(index_path=sp('image_query_batch.html'),
